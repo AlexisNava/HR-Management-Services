d79b5fa567f3be5bda04e2dc107fb358d7aca5b3
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-12 19:54:55 -0500

    Configured morgan in express app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+require('dotenv').config();
 
 const app = express();
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Use Middlewares
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 module.exports = app;
