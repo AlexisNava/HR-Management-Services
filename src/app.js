5a28a209eae9c8a2726b726176f66e064e98d52a
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-13 09:47:54 -0500

    Removed morgan logger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 
 require('dotenv').config();
 
@@ -13,16 +12,10 @@ const { root, notFound, errorHandler } = require('./middlewares');
 
 const app = express();
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
 // Use Middlewares
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-if (NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
 // Test DB Connection
 hrManagementServicesDB
