148bbfe9e00d7cba6ab426d90e7e27e8bc6cce78
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-15 11:10:10 -0500

    Added registerAdministrator method

--- src/services/auth/controller.js
+++ src/services/auth/controller.js
@@ -1,12 +1,41 @@
 // Prisma Client
 const { prisma } = require('../../db/generated/prisma-client');
 
-async function registerAdministrator({ password, personalInformation }) {
+/**
+ * Create a new administrator
+ * @param {Object} newAdministrator - Administrator Model
+ */
+async function registerAdministrator(newAdministrator) {
+  const { password } = newAdministrator;
+  const {
+    email,
+    names,
+    lastName,
+    mothersName,
+    phoneNumber,
+  } = newAdministrator.personalInformation;
+
   // Encrypt Password
 
   try {
     // Register new Admin
-    prisma.createAdministrator({});
+    const response = await prisma.createAdministrator({
+      user: {
+        password,
+        isAdmin: true,
+        personalInformation: {
+          email,
+          names,
+          lastName,
+          mothersName: mothersName || null,
+          phoneNumber: phoneNumber || null,
+        },
+      },
+    });
+
+    console.log('response', response);
+
+    return response;
   } catch (error) {
     return error;
   }
