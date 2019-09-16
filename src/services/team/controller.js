847ab19c409316120d23714c05d56e2b4800df15
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-16 08:16:21 -0500

    Changed the getAllTeams api

--- src/services/team/controller.js
+++ src/services/team/controller.js
@@ -4,7 +4,7 @@ const { prisma } = require('../../db/generated/prisma-client');
 async function getAllTeams(validatedToken) {
   const { id } = validatedToken;
 
-  const response = await prisma.administrator({ id }).teams();
+  const response = await prisma.teams({ where: { admin: id } });
 
   return response;
 }
