8c2d94e396f4b03d4b40edb7e0b1e9e019a6f5f5
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-17 08:55:07 -0500

    Added cors configuration

--- src/app.js
+++ src/app.js
@@ -20,8 +20,16 @@ app.disable('x-powered-by');
 
 // Use Middlewares
 app.use(pino());
-app.use(cors());
 app.use(bodyParser.json());
+
+// CORS Congiguration
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+});
 
 app.get('/', root);
 app.use('/api/auth', AuthServices);
