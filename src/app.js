48947103ccb8ad40e6ab8f1577a02251e68134ca
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-18 01:30:40 -0500

    Disabled eslint rule for all app.js file

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger');
@@ -28,6 +30,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   );
+
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
