dbfba91f004e3b25dd878076156465098e7451e4
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2019-09-18 20:08:12 -0500

    Resolved eslint issues

--- src/db/generated/prisma-client/index.js
+++ src/db/generated/prisma-client/index.js
@@ -1,37 +1,37 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var prisma_lib_1 = require("prisma-client-lib");
-var typeDefs = require("./prisma-schema").typeDefs;
+/* eslint-disable camelcase */
+Object.defineProperty(exports, '__esModule', { value: true });
+const prisma_lib_1 = require('prisma-client-lib');
+const { typeDefs } = require('./prisma-schema');
 
-var models = [
+const models = [
   {
-    name: "User",
-    embedded: false
+    name: 'User',
+    embedded: false,
   },
   {
-    name: "Administrator",
-    embedded: false
+    name: 'Administrator',
+    embedded: false,
   },
   {
-    name: "Team",
-    embedded: false
+    name: 'Team',
+    embedded: false,
   },
   {
-    name: "Position",
-    embedded: false
+    name: 'Position',
+    embedded: false,
   },
   {
-    name: "Employee",
-    embedded: false
+    name: 'Employee',
+    embedded: false,
   },
   {
-    name: "Report",
-    embedded: false
-  }
+    name: 'Report',
+    embedded: false,
+  },
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466`
+  endpoint: `http://localhost:4466`,
 });
 exports.prisma = new exports.Prisma();
